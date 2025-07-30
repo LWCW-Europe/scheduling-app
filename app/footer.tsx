7639508cfa015605d28437159bac319ef6a6448b
Leonardo Taglialegne <cmt.miniBill@gmail.com>
2025-07-30 21:15:46 +0200

    Simplify footer

--- app/footer.tsx
+++ app/footer.tsx
@@ -7,13 +7,10 @@ export default function Footer() {
 
   return (
     <footer className="sm:fixed bottom-0 left-0 right-0 bg-gray-50 border-t border-gray-200 py-2 z-20">
-      <div className="px-3 flex justify-between items-center">
-        <span className="text-xs text-gray-500">Version: {commitHash}</span>
+      <div className="px-3 flex justify-between items-center text-xs text-gray-500">
+        Version: {commitHash}
         {footerRightContent && (
-          <div
-            className="text-xs text-gray-500"
-            dangerouslySetInnerHTML={{ __html: footerRightContent }}
-          />
+          <div dangerouslySetInnerHTML={{ __html: footerRightContent }} />
         )}
       </div>
     </footer>
