ff9acefa570ba0800a974c18080b897dd688130f
Omar Kohl <omarkohl@posteo.net>
2025-08-06 22:37:46 +0100

    style: 'prettier' changes to test file

--- tests/proposals.spec.ts
+++ tests/proposals.spec.ts
@@ -1,8 +1,12 @@
 import { test, expect } from "@playwright/test";
 
-test("should auto-focus the title input for new proposals", async ({ page }) => {
+test("should auto-focus the title input for new proposals", async ({
+  page,
+}) => {
   await page.goto("/Test-Conference/proposals/new");
-  const focusedElement = await page.evaluateHandle(() => document.activeElement);
+  const focusedElement = await page.evaluateHandle(
+    () => document.activeElement
+  );
   const placeholder = await focusedElement.getAttribute("placeholder");
   expect(placeholder).toBe("Enter a clear, descriptive title");
 });
