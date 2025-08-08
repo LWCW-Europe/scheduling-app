d45060ea720451bbdc9ea22f568897e14523a1fe
Omar Kohl <omarkohl@posteo.net>
2025-08-08 20:23:16 +0100

    fix: change column title from 'Your votes' to 'Your vote'
    The user casts one vote per proposal so plural would be incorrect.

--- app/[eventSlug]/proposals/proposal-table.tsx
+++ app/[eventSlug]/proposals/proposal-table.tsx
@@ -347,7 +347,7 @@ export function ProposalTable({
                 scope="col"
                 className="w-[10%] px-4 lg:px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider"
               >
-                Your votes
+                Your vote
               </th>
               <th
                 scope="col"
