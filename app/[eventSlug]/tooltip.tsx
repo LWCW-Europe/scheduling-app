2f3cdfd495c09affa7c4cc5a1df0f73f141bc809
Velizar Hristov <velizar.hs@gmail.com>
2025-08-26 12:56:17 +0300

    Increase the width of tooltips, especially because of session blocks.

--- app/[eventSlug]/tooltip.tsx
+++ app/[eventSlug]/tooltip.tsx
@@ -93,7 +93,7 @@ export function Tooltip(props: {
         role="tooltip"
         ref={refs.setFloating}
         style={{ position: strategy, top: y ?? 0, left: x ?? 0 }}
-        className="z-40 max-w-lg w-72 whitespace-normal rounded bg-white px-2 py-1 border shadow-md border-gray-100"
+        className="z-40 max-w-lg w-120 whitespace-normal rounded bg-white px-2 py-1 border shadow-md border-gray-100"
         suppressHydrationWarning={suppressHydrationWarning}
         {...getFloatingProps()}
       >
