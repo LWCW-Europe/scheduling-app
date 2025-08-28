c53908c935003a2cb2af5b1eb75c02d482450405
Omar Kohl <omarkohl@posteo.net>
2025-08-28 18:29:14 +0200

    fix: don't display fixed footer on phones in landscape mode
    #288

--- app/layout.tsx
+++ app/layout.tsx
@@ -49,7 +49,7 @@ export default function RootLayout({
           <main
             className={clsx(
               "lg:px-24 sm:px-10 p-6 flex-1",
-              CONSTS.MULTIPLE_EVENTS ? "py-24 sm:pb-16" : "pt-12 sm:pb-16"
+              CONSTS.MULTIPLE_EVENTS ? "py-24 lg:pb-16" : "pt-12 lg:pb-16"
             )}
           >
             {children}
